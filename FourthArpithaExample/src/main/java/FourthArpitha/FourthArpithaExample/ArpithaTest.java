1466b69f4dd49435905aad729c483c7deb307713
akundurt <akundurt@GDCPHXDT118456.igategegdc.com>
2018-02-27 20:26:04 +0530

    this is my first Commit

---
+++ FourthArpithaExample/src/main/java/FourthArpitha/FourthArpithaExample/ArpithaTest.java
@@ -0,0 +1,12 @@
+package FourthArpitha.FourthArpithaExample;
+
+public class ArpithaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my First Push");
+
+	}
+
+}
